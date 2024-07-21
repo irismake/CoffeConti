2485394046ae7f5f712acf72c593d2a43fd70fc3
irismake <iris3455@gmail.com>
2024-07-21 20:52:58 +0900

    style : User page UI/UX

--- lib/screen/tabs/user_page.dart
+++ lib/screen/tabs/user_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../components/button/category_thumb.dart';
 import '../../components/list/user_place.dart';
 
 class UserPage extends StatelessWidget {
@@ -8,218 +9,210 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Color backgroundColor =
-        Theme.of(context).primaryColor.withOpacity(0.2);
-
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Colors.pink,
-        //title: Text('My App'),
         actions: <Widget>[
           IconButton(
             icon: Image.asset(
               'assets/icons/button_more.png',
               height: 24.h,
             ),
-            onPressed: () {
-              // do something
-            },
+            onPressed: () {},
           )
         ],
       ),
       body: Column(
         children: [
-          Container(
-            child: Padding(
-              padding:
-                  EdgeInsets.only(left: 16.0.w, right: 16.0.w, bottom: 20.0.h),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 18.0.w),
-                    child: Container(
-                      height: 82.0.h,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: ClipOval(
-                        child: Image.asset(
-                          'assets/images/image_user_profile_1.png',
-                        ),
+          Padding(
+            padding:
+                EdgeInsets.only(left: 16.0.w, right: 16.0.w, bottom: 20.0.h),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 18.0.w),
+                  child: Container(
+                    height: 82.0.h,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/images/image_user_profile_1.png',
                       ),
                     ),
                   ),
-                  Container(
-                    height: 60.0.h,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'heenano',
-                          style: TextStyle(
-                            fontFamily: 'PretendardRegular',
-                            fontSize: 22.sp,
-                            fontWeight: FontWeight.w800,
-                            color: Color(0xff343A40),
-                            height: 1.43,
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            // 레벨 가이드 페이지 이동
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(6.0),
-                            ),
-                            // 패딩을 0으로 설정
-                            minimumSize: Size.zero, // 최소 크기를 0으로 설정
-                            tapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap, // 터치 대상 크기 조정
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10.0.w,
-                              vertical: 4.0.h,
-                            ),
-                            backgroundColor: Color(0xFF3D3332),
-                            elevation: 0,
-                          ),
-                          child: Text(
-                            '@ 따끈따끈한 뉴비',
-                            style: TextStyle(
-                              fontFamily: 'PretendardRegular',
-                              fontSize: 10.sp,
-                              fontWeight: FontWeight.w600,
-                              color: Theme.of(context).primaryColor,
-                              height: 1.43,
-                            ),
-                          ),
-                        ),
-                      ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'heenano',
+                      style: TextStyle(
+                        fontFamily: 'PretendardRegular',
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.w800,
+                        color: Color(0xff212529),
+                        height: 1.43,
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                    SizedBox(
+                      height: 6.0.h,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // 레벨 가이드 페이지 이동
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                        minimumSize: Size.zero,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10.0.w,
+                          vertical: 4.0.h,
+                        ),
+                        backgroundColor: Color(0xff02343F),
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        '@ 따끈따끈한 뉴비',
+                        style: TextStyle(
+                          fontFamily: 'PretendardRegular',
+                          fontSize: 10.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFF0EDCC),
+                          height: 1.43,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: Container(
-              color: Color(0xfff8f8f8),
-              child: Padding(
+          Column(
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
                 padding: EdgeInsets.symmetric(
-                  vertical: 10.0.h,
                   horizontal: 16.0.w,
                 ),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '나의 리뷰',
-                          style: TextStyle(
-                            fontFamily: 'PretendardRegular',
-                            fontSize: 18.sp,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black,
-                            height: 1.56,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 2.0.h, horizontal: 2.0.w),
-                          ),
-                          child: Text(
-                            '더보기',
-                            style: TextStyle(
-                              fontFamily: 'PretendardRegular',
-                              fontSize: 14.sp,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xff868E96),
-                              height: 1.5,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 110,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () async {},
-                            child: UserPlace(
-                              cafeName: '커피스토어',
-                            ),
-                          );
-                        },
+                    Text(
+                      '나의 리뷰',
+                      style: TextStyle(
+                        fontFamily: 'PretendardRegular',
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w800,
+                        color: Color(0xff343A40),
+                        height: 1.56,
                       ),
                     ),
-                    SizedBox(
-                      height: 28.0.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '즐겨찾기',
-                          style: TextStyle(
-                            fontFamily: 'PretendardRegular',
-                            fontSize: 18.sp,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black,
-                            height: 1.56,
-                          ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 2.0.h, horizontal: 2.0.w),
-                          ),
-                          child: Text(
-                            '더보기',
-                            style: TextStyle(
-                              fontFamily: 'PretendardRegular',
-                              fontSize: 14.sp,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xff868E96),
-                              height: 1.5,
-                            ),
-                          ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 2.0.h, horizontal: 2.0.w),
+                      ),
+                      child: Text(
+                        '더보기',
+                        style: TextStyle(
+                          fontFamily: 'PretendardRegular',
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff868E96),
+                          height: 1.5,
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 110,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () async {},
-                            child: UserPlace(
-                              cafeName: '커피스토어',
-                            ),
-                          );
-                        },
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
+              SizedBox(
+                height: 130,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () async {},
+                      child: UserPlace(
+                        cafeName: '커피스토어',
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 12.0.h,
+                  horizontal: 16.0.w,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '즐겨찾기',
+                      style: TextStyle(
+                        fontFamily: 'PretendardRegular',
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w800,
+                        color: Color(0xff343A40),
+                        height: 1.56,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 2.0.h, horizontal: 2.0.w),
+                      ),
+                      child: Text(
+                        '더보기',
+                        style: TextStyle(
+                          fontFamily: 'PretendardRegular',
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff868E96),
+                          height: 1.5,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 130,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () async {},
+                      child: CategoryThumb(
+                        index: index,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       ),
