6796e1851d583cb788dd49ad0e480a48b81fe4e9
irismake <iris3455@gmail.com>
2024-07-30 22:37:50 +0900

    style : Place picker page proto UI/UX

--- lib/components/popup/cafe_tutorial.dart
+++ lib/components/popup/cafe_tutorial.dart
@@ -1,134 +1,158 @@
-import 'package:coffeeconti/components/constants/gaps.dart';
-import 'package:coffeeconti/data/cafe_data.dart';
-import 'package:coffeeconti/screen/map/map.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
-import '../constants/screenSize.dart';
-import '../constants/sizes.dart';
+import '../ui/keyword.dart';
+import '../ui/star_rating.dart';
 
 class CafeTutorial extends StatelessWidget {
-  final cafeName;
+  final int index;
+  final String name;
 
   CafeTutorial({
     super.key,
-    required this.cafeName,
+    required this.index,
+    required this.name,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      // behavior: HitTestBehavior.opaque,
       onTap: () {
         print('gg');
       },
-
-      // child: Padding(
-      //   padding: EdgeInsets.only(
-      //     top: cafeTutorialTopPosition(context),
-      //     bottom: cafeTutorialBottomPosition(context),
-      //   ),
-      child: Container(
-        margin: EdgeInsets.all(
-          CafeTutorialMarginSize(context),
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 5.0,
-              offset: Offset(0, 5),
-            ),
-          ],
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 18.0,
         ),
         child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                // width: 110,
-                color: Colors.grey[200],
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 25,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 16.0.w),
+                  child: Container(
+                    width: 70.0,
+                    height: 70.0,
+                    decoration: ShapeDecoration(
+                      color: Color(0xffF1F3F5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: index == 0
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              'assets/images/IMG_4498.png',
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        : SizedBox.shrink(),
+                  ),
                 ),
-                child: Column(
+                Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 5),
-                      height: 30,
-                      // color: Colors.amber,
-                      alignment: Alignment.centerLeft,
-                      child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
-                          ),
-                          child: Text(
-                            limitTextLength(cafeName, 15),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
+                    Text(
+                      limitTextLength(name, 15),
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
                       ),
                     ),
                     Row(
                       children: [
-                        Icon(Icons.access_time, size: 20),
-                        SizedBox(width: 5),
-                        DefaultTextStyle(
+                        Text(
+                          "4.3",
                           style: TextStyle(
-                            fontSize: 15,
-                            //fontFamily: ,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w700,
+                            color: Theme.of(context).primaryColor,
+                            height: 1.3,
                           ),
-                          child: Text(
-                            '',
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 4.0.w),
+                          child: SizedBox(
+                            height: 16.w,
+                            child: StarRating(
+                              rating: 4.3,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "(26)",
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xff868E96),
+                            height: 1.3,
                           ),
                         ),
                       ],
                     ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(
+                        children: [
+                          Keyword(keywordName: '공부'),
+                          SizedBox(width: 6.0),
+                          Keyword(keywordName: '디저트'),
+                        ],
+                      ),
+                    ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Icon(
-                          Icons.location_on,
-                          size: 20,
-                        ),
-                        SizedBox(width: 5),
-                        DefaultTextStyle(
-                          style: const TextStyle(
-                            fontSize: 15,
-                            //fontFamily: ,
-                            fontWeight: FontWeight.w600,
+                        Text(
+                          "30m",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
                             color: Colors.black,
                           ),
-                          child: Text(
-                            "30m",
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                          height: 12.0,
+                          child: SvgPicture.asset(
+                            'assets/images/vertical_vector.svg',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Text(
+                          "서울 성북구 안암동 3가",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF495057),
                           ),
                         ),
                       ],
                     ),
                   ],
                 ),
+              ],
+            ),
+            SizedBox(
+              // color: Colors.pink,
+              height: 18.0,
+              child: Image.asset(
+                'assets/icons/icon_bookmark_fill.png',
+                fit: BoxFit.contain,
               ),
             ),
           ],
         ),
       ),
-      //),
     );
   }
 
--- lib/screen/place_picker/place_picker_page.dart
+++ lib/screen/place_picker/place_picker_page.dart
@@ -1,10 +1,231 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../components/constants/screenSize.dart';
+import '../../components/popup/cafe_tutorial.dart';
+
+import '../../components/widgets/search_keyword_widget.dart';
+import 'place_list_view_page.dart';
 
 class PlacePickerPage extends StatelessWidget {
   const PlacePickerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<String> _placeName = [
+      '커피스토어',
+      '스타벅스 종암 DT점',
+      '당가라 과자점',
+      '코스모',
+      '브라운 헤이브',
+      'coffeeconti'
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leadingWidth: 100,
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        flexibleSpace: Padding(
+          padding: EdgeInsets.only(
+            top: ViewPaddingTopSize(context) + 10.0.h,
+            left: 16.0.w,
+            bottom: 10.0.h,
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/image_logo.png',
+              ),
+            ],
+          ),
+        ),
+        shape: Border(
+          bottom: BorderSide(
+            color: Color(0xffb2b2b2),
+            width: 1,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // SizedBox(
+            //   height: ViewPaddingTopSize(context) + 10.0.h,
+            // ),
+            // Padding(
+            //   padding: EdgeInsets.symmetric(
+            //     vertical: 10.0.h,
+            //   ),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     crossAxisAlignment: CrossAxisAlignment.center,
+            //     children: [
+            //       Text(
+            //         '랭킹',
+            //         style: TextStyle(
+            //           fontFamily: 'PretendardRegular',
+            //           fontSize: 20.sp,
+            //           fontWeight: FontWeight.w700,
+            //           color: Colors.black,
+            //           height: 1.56,
+            //         ),
+            //       ),
+            //       Text(
+            //         '추가',
+            //         style: TextStyle(
+            //           fontFamily: 'PretendardRegular',
+            //           fontSize: 14.sp,
+            //           fontWeight: FontWeight.w600,
+            //           color: Color(0xff343A40),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '내가 북마크한 리스트',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18.0.sp,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print('더보기');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PlaceListViewPage()),
+                        );
+                      },
+                      child: Text(
+                        '더보기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0.sp,
+                          color: Color(0xff868E96),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 12.0, bottom: 24.0),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 2.0.h,
+                    height: 0,
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                  child: Expanded(
+                    child: ListView.separated(
+                      padding: EdgeInsets.symmetric(vertical: 0.0),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _placeName.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () async {},
+                          child: CafeTutorial(
+                            name: '${_placeName[index]}',
+                            index: index,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 1.0,
+                          color: Color(0xFFDEE2E6),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '근처 핫플레이스',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18.0.sp,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print('더보기');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PlaceListViewPage()),
+                        );
+                      },
+                      child: Text(
+                        '더보기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0.sp,
+                          color: Color(0xff868E96),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 12.0, bottom: 24.0),
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 2.0.h,
+                    height: 0,
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                  child: Expanded(
+                    child: ListView.separated(
+                      padding: EdgeInsets.symmetric(vertical: 0.0),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _placeName.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () async {},
+                          child: CafeTutorial(
+                            name: '${_placeName[index]}',
+                            index: index,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 1.0,
+                          color: Color(0xFFDEE2E6),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
