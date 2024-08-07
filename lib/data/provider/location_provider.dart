450da7399d01f7f610b868e8ffca848b2bae8e9d
irismake <iris3455@gmail.com>
2024-08-07 15:39:34 +0900

    fix : Display Kakao Map on the map page

--- lib/data/provider/location_provider.dart
+++ lib/data/provider/location_provider.dart
@@ -1,22 +1,27 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import '../../components/popup/app_setting_popup.dart';
 import '../../components/popup/error_message_popup.dart';
 
 class LocationProvider extends ChangeNotifier {
-  Position? _position;
-  Position? get position => _position;
+  LatLng? _position;
+  LatLng? get position => _position;
 
-  Future<Position?> getCurrentPosition(BuildContext context) async {
+  Future<LatLng?> getInitialPosition(BuildContext context) async {
+    if (_position != null) {
+      return _position; // 이미 위치를 가져왔다면 캐시된 값을 반환
+    }
+
     PermissionStatus status = await Permission.location.request();
     if (status.isGranted) {
       try {
-        _position = await Geolocator.getCurrentPosition(
+        Position currentPosition = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.best,
         );
+        _position = LatLng(currentPosition.latitude, currentPosition.longitude);
       } catch (e) {
         print('Dont get currentPosition');
         _errorPopup(context);
@@ -35,7 +40,7 @@ void _showPermissionPopup(BuildContext context) {
     barrierDismissible: false,
     context: context,
     builder: (BuildContext context) {
-      return ShowAppSettingMessage(); // Show the MyPopup widget as the content of the dialog
+      return ShowAppSettingMessage();
     },
   );
 }
@@ -45,8 +50,7 @@ void _errorPopup(BuildContext context) {
     barrierDismissible: true,
     context: context,
     builder: (BuildContext context) {
-      return ShowErrorMessage(
-          context); // Show the MyPopup widget as the content of the dialog
+      return ShowErrorMessage(context);
     },
   );
 }
