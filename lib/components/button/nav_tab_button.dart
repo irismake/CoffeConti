d760edc5ea7bee5480185f57cab837f5bfeaea00
irismake <iris3455@gmail.com>
2024-08-10 21:38:35 +0900

    fix : Bottom navigation bar status sharing & style modification

--- lib/components/button/nav_tab_button.dart
+++ lib/components/button/nav_tab_button.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NavTabButton extends StatelessWidget {
+  final bool isSelected;
+  final String iconName;
+  final VoidCallback onTap;
+
   const NavTabButton({
     super.key,
     required this.isSelected,
     required this.iconName,
+    required this.onTap,
   });
-
-  final bool isSelected;
-  final String iconName;
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      isSelected
-          ? 'assets/icons/${iconName}_fill.png'
-          : 'assets/icons/${iconName}_light.png',
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: SizedBox(
+          child: Image.asset(
+            'assets/icons/${iconName}_fill.png',
+            color: isSelected ? Color(0xffFFBE98) : Colors.grey[600],
+            height: 22.h,
+          ),
+        ),
+      ),
     );
   }
 }
